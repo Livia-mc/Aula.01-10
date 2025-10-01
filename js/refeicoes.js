8ccbbd398f4500e135f3a6ebb876e641bf1670bc
Livia-mc <livia.cassiano.ifpr.2023@gmail.com>
2025-10-01 15:41:07 -0300

    já me perdi nas versoes

--- js/refeicoes.js
+++ js/refeicoes.js
@@ -1,12 +1,13 @@
-var cafe=[], c
+var cafe=[], c, almoco=[], a, jantar=[], j
 
-cafe[0]=alert(prompt("O que quer para o café da manhã?"))
 
-    if(cafe[0]==""){
+
+cafe[0]=(prompt("O que quer para o café da manhã?"))
+
+    while(cafe[0]==""){
     alert("Você tem que escolher ao menos um elemento do nosso cardápio")
-    cafe[0]=alert(prompt("O que quer para o café da manhã?"))
+    cafe[0]=(prompt("O que quer para o café da manhã?"))
 }
-
 
 do{
     c=prompt("O que mais? ")
@@ -17,13 +18,78 @@ do{
     else{
     cafe.push(c)}
 }
+
 while(c!=null)
 
-for(var i=0; i<cafe.length-1; i++){
-    document.write("<h2>"+cafe[i]+"</h2>")
 
 
+
+almoco[0]=(prompt("O que quer para o almoço?"))
+
+    while(almoco[0]==""){
+    alert("Você tem que escolher ao menos um elemento do nosso cardápio")
+    almoco[0]=(prompt("O que quer para o almoço?"))
 }
 
+do{
+    a=prompt("O que mais? ")
+
+    if(a==""){
+    alert("Digite algo, se quer terminar o pedido aperte em 'cancelar'")}
+
+    else{
+    almoco.push(a)}
+
+}
+while(a!=null)
+
+
+
+
+
+jantar[0]=(prompt("O que quer para o jantar?"))
+
+    while(jantar[0]==""){
+    alert("Você tem que escolher ao menos um elemento do nosso cardápio")
+    jantar[0]=(prompt("O que quer para o jantar?"))
+}
+
+do{
+    j=prompt("O que mais? ")
+
+    if(j==""){
+    alert("Digite algo, se quer terminar o pedido aperte em 'cancelar'")}
+
+    else{
+    jantar.push(j)}
+
+}
+while(j!=null)
+
+
+
+
+
+
+
+
+    document.write("<h1>CARDÁPIO</h1>")
+
+    document.write("<h2>Seu café da manhã</h2>")
+for(var i=0; i<cafe.length-1; i++){
+    document.write("<ul><li>"+cafe[i]+"</li></ul>")
+}
+
+
+    document.write("<h2>Seu almoço</h2>")
+for(var i=0; i<almoco.length-1; i++){
+    document.write("<ul><li>"+almoco[i]+"</li></ul>")
+}
+
+
+    document.write("<h2>Seu jantar</h2>")
+for(var i=0; i<jantar.length-1; i++){
+    document.write("<ul><li>"+jantar[i]+"</li></ul>")
+}
 
 
