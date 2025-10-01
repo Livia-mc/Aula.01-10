bcea4a0fd237ddcdccfb28cd7d9f934ead64b13c
Livia-mc <livia.cassiano.ifpr.2023@gmail.com>
2025-10-01 14:29:54 -0300

    segunda parte
    não sei oq está acontecendo mas não está dando certo

--- js/refeicoes.js
+++ js/refeicoes.js
@@ -1,10 +1,16 @@
 var cafe=[], c
 
+cafe[0]=alert(prompt("O que quer para o café da manhã? "))
+
+if(cafe[0]==null){
+    alert=("Você tem que escolher ao menos um elemento do nosso cardápio")
+}
+
 do{
-    c=alert(prompt("O que quer para o café da manhã?"))
+    c=alert(prompt("O que mais? "))
     cafe.push(c)
-    if(c==""){
-    alert("Digite algo")
+      if(c==""){
+      alert("Digite algo")
     }
 }
 while(c!=null) {
