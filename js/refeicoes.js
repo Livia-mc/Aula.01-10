c554522eb4c48844826a20fde8975d6e6fdb4f98
Livia-mc <livia.cassiano.ifpr.2023@gmail.com>
2025-10-01 14:39:07 -0300

    terceira parte

--- js/refeicoes.js
+++ js/refeicoes.js
@@ -1,21 +1,26 @@
 var cafe=[], c
 
-cafe[0]=alert(prompt("O que quer para o café da manhã? "))
+cafe[0]=alert(prompt("O que quer para o café da manhã?"))
 
-if(cafe[0]==null){
+    if(cafe[0]==null){
     alert=("Você tem que escolher ao menos um elemento do nosso cardápio")
 }
 
+
 do{
-    c=alert(prompt("O que mais? "))
+    c=prompt("O que mais? ")
     cafe.push(c)
-      if(c==""){
-      alert("Digite algo")
-    }
+    if(c==""){
+    alert("Digite algo ")
 }
-while(c!=null) {
+}
+while(f!=null)
 
 for(var i=0; i<cafe.length-1; i++){
     document.write("<h2>"+cafe[i]+"</h2>")
-    }
+
+
 }
+
+
+
